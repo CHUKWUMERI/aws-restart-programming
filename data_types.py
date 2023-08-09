5173fa92a355ba2163b7c52e1c3f7bde681c1ea1
Chukwumeri <ekomepreciousgift@gmail.com>
2023-08-10 00:44:35 +0100

    Initial Commit to the GitHub Repo

---
+++ data_types.py
@@ -0,0 +1,32 @@
+1 # Integer - It is a whole number
+2.0 # Float - A number with a decimal point
+True # Boolean
+False # Boolean - A true or false statement
+"Hello World" # String - Any character that is enclosed by quotation marks (single, double or triple)
+'900' # String - because it is enclosed by quotation marks
+
+# Primary Data types
+owner = "Precious-Gift Ekome" # Python does not see space as a delimiter
+date = "August 8th 2023"
+version = 1.0
+final_version = False
+
+# Secondary Data types - Composed of the primary datatypes and sometimes themselves 
+# Each element is spearated from the other by a comma
+[12, "Python", "is", "case sensistive"] # List - A collection of characters enclosed by square brackets; lists are mutable
+(12, "Python", "is", "case sensistive") # Tuple - An immutable collection of characters enclosed by parenthesis
+{"aws-restart": "a 12 weeks course introducing learners into the cloud",
+ "date": "August 8 2023", True: "A boolean data type", True: "Keys must be unique"} # Dictionary - A mutable collection of key-value pairs enclosed by curly braces. Each key and value is linked by a colon
+
+
+# Create a list with 5 elements, float, integer, boolean, 2 strings
+[10, 3.5, False, "Python", "Case SENSITIVE"]
+
+# Create a dictionary with five keys and values
+{1: 40, "hello": 20, "hi": 5, True: 9, "python": 2}
+# Values can be any data type, however keys are restricted to integers, floats, boolean, string and tuples
+
+
+# Print helps you to output a value to the standard output
+print(date)
+print("My name is Python and I am a great programming language to learn!")
